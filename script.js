465ca9e8c083b5feac5e6bb8b2ec21d2de242642
saw-zk <s221035@stn.nagaokaut.ac.jp>
2025-04-14 15:57:12 +0900

    JSの追加

---
+++ script.js
@@ -0,0 +1,6 @@
+// メッセージを書き換える関数
+function changeMessage() { //関数（処理のまとまり）を定義
+    const messageElement = document.getElementById("message"); //HTML内のid="message" の要素を取得
+    messageElement.textContent = "学びながら成長中です！"; //要素の中身の文字を変更
+  }
+  
